da8dd214fef2e1d177060153062f16be3522bd7f
Jen <jenniferanndcb@gmail.com>
2018-05-16 12:41:59 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,23 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+console.log(chocolateBars)
+
+function addElementToBeginningOfArray(chocolateBars,candyString){
+  return [element ...array]
+};
+
+function destructivelyAddElementToBeginningOfArray (array,element){
+  array.unshift(element)
+  return array 
+};
+
+function addElementToEndOfArray (array,element){
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray (array,element){
+  return [array.push]
+}
+
+function accessElementInArray(array, index){
+  return array[index]
+}
